975884324c057a44ce2a74cd85b0dd8d4e84f2e7
crisluda <crisluda2244@gmail.com>
2023-02-24 02:05:00 -0800

    1 and 2

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isdigit - this is the main function
+ * @c: c is a int
+ * discription - the starting point of the program
+ * Return: the main have to return 0
+ */
+int _isdigit(int c)
+{
+	if (c <= 56 && c >= 48)
+	{
+	return (1);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - this is the main function
+ * @a: a is an int
+ * @b: b is an int
+ * discription - the starting point of the program
+ * Return: the main have to return 0
+ */
+int mul(int a, int b)
+{
+	int total;
+
+	total = a * b;
+	return (total);
+}
