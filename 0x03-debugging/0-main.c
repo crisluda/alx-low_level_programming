454310468361cb3cfcbb2e4567eb8b57a3197ad1
crisluda <crisluda2244@gmail.com>
2023-02-27 07:50:42 -0800

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+iclude "main.h"
+/**
+ * main - this is the main function
+ * discription - the starting point of the program
+ * Return: the main have to return 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
