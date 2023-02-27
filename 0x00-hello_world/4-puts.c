f71946798af33a51e9a2602134cd1a1809d79948
crisluda <crisluda2244@gmail.com>
2023-02-27 02:14:29 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main()
+/**
+ * main - this is the main function
+ * discription - the starting point of the program
+ * Return: the main have to return 0
+ */
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
