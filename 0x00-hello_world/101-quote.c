06df0ce42b218a51558259af7b618b8e45038f81
crisluda <crisluda2244@gmail.com>
2023-02-27 02:41:33 -0800

    Write a C program that prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - this is the main function
+ * discription - the starting point of the program
+ * Return: the main have to return 0
+ */
+int main (void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
