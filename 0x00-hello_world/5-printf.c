e2313971cfdca3d670a71d5532752580f86fb0d5
crisluda <crisluda2244@gmail.com>
2023-02-27 02:23:31 -0800

    wqHello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * discription - the starting point of the program
+ * Return: the main have to return 0
+ */
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
