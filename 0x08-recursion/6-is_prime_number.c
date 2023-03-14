109386fd078a406310257500753af322bbc58156
crisluda <crisluda2244@gmail.com>
2023-03-14 06:41:09 -0700

    Write a function that returns 1 if the input integer is a prime number, otherwise return 0.

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+int actual_prime(int n, int i);
+/**
+ * is_prime_number - this is the main function
+ * @n: is an int
+ * discription - the starting point of the program
+ * Return: the main have to return 0
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+	return (0);
+	}
+	return (actual_prime(n, n - 1));
+}
+/**
+ * actual_prime - this is the main function
+ * @i: is an int
+ * @n: is an int
+ * discription - the starting point of the program
+ * Return: the main have to return 0
+ */
+int actual_prime(int n, int i)
+{
+	if (i == 1)
+	{
+	return (1);
+	}
+	if (n % i == 0 && i > 0)
+	{
+	return (0);
+	}
+	return (actual_prime(n, i - 1));
+}
