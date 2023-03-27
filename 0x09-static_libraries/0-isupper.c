7919613d1e13329e8f4955c54bde04b1a269847e
crisluda <crisluda2244@gmail.com>
2023-03-27 04:41:33 -0700

    Static Librairies$ ar -t libmy.a

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - uppercase letters
+ * @c: char to check
+ * Return: the main have to return 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	return (1);
+	else
+	return (0);
+}
