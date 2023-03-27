02c11d87c94a7bc0fb3a20ce37491ce2329b5b1a
crisluda <crisluda2244@gmail.com>
2023-03-27 05:38:44 -0700

    first

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - this is the main function
+ * discription - the starting point of the program
+ * Return: the main have to return 0
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+	int i;
+	for (i = 1; i <= 98; i++)
+	{
+	printf("%d",i);
+	}
+	}
+}
